3711555172ee12760947429e7ff86a61e00645e8
Martin Alexandersson <marale@kth.se>
2020-01-29 10:14:46 +0100

    Fiddled around with the pint package, but didn't get it to work with pandas so I dished it!

---
+++ rolldecay/__init__.py
@@ -0,0 +1,8 @@
+#from pint import UnitRegistry
+#ureg = UnitRegistry()
+#Quantity = ureg.Quantity
+#radian = ureg.radian
+#second = ureg.second
+#dimensionless = ureg.Quantity(1,units='dimensionless').units
+#nondimensional = dimensionless
+
