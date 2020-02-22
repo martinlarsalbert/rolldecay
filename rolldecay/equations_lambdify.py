5027a78eee66afd6fb698b41b8da121f229a0bc5
Martin Alexandersson <marale@kth.se>
2020-02-22 15:38:25 +0100

    Rmeoved some stuff.

--- rolldecay/equations_lambdify.py
+++
@@ -1,6 +0,0 @@
-from rolldecay import equations
-from rigidbodysimulator.substitute_dynamic_symbols import lambdify
-
-calculate_acceleration = lambdify(equations.acceleration_equation.rhs)
-calculate_velocity = lambdify(equations.velocity_equation.rhs)
-
