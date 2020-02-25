c92a4b60a4c75524fc52991549b4d775accdf529
Martin Alexandersson <marale@kth.se>
2020-02-25 14:37:16 +0100

    OK, worked a bit on the latex equations.

--- latex_helpers/pylatex_extenders.py
+++ latex_helpers/pylatex_extenders.py
@@ -1,6 +1,7 @@
 from pylatex.base_classes.containers import Container
 from pylatex import NoEscape
 import sympy as sp
+from sympy.physics.vector.printing import vlatex
 
 
 class GeneralContainer(Container):
@@ -27,7 +28,9 @@ class Equation(Container):
     _latex_name = 'equation'
 
     def __init__(self, sympy_equation, label=None):
-        super().__init__(data=NoEscape(sp.latex(sympy_equation)))
+        #super().__init__(data=NoEscape(sp.latex(sympy_equation)))
+        super().__init__(data=NoEscape(vlatex(sympy_equation)))
+
         self.label = label
 
     def dumps(self):
