ab5a6b0f66cbaf026c5689c5e701d90604c302a9
Martin Alexandersson <marale@kth.se>
2020-03-19 11:19:32 +0100

    Rewrote the equations a bit.

---
+++ rolldecay/equations.py
@@ -0,0 +1,62 @@
+import sympy as sp
+from rolldecayestimators.symbols import *
+import rolldecayestimators.direct_linear_estimator as direct_linear_estimator
+
+# General roll motion equation according to Himeno:
+lhs = A_44*phi_dot_dot + B_44 + C_44
+rhs = M_44
+roll_equation_himeno = sp.Eq(lhs=lhs, rhs=rhs)
+
+# No external forces (during roll decay)
+roll_decay_equation_general_himeno = roll_equation_himeno.subs(M_44,0)
+
+
+
+# General equation for roll motion:
+lhs = I*phi_dot_dot + B*phi_dot + m*g*GZ
+rhs = M_x
+roll_equation_general = sp.Eq(lhs=lhs, rhs=rhs)
+
+# No external forces (during roll decay)
+roll_decay_equation_general = roll_equation_general.subs(M_x,0)
+
+##### Linear
+linear_stiffness_equation = sp.Eq(GZ,GM*phi)
+
+subs = [
+    (GZ, sp.solve(linear_stiffness_equation, GZ)[0]),
+]
+roll_decay_linear_equation = roll_decay_equation_general.subs(subs)
+
+linear_damping_equation = sp.Eq(B/I,zeta)
+
+
+# Solve the diff equation by introducing helper variables:
+phi_old,p_old = me.dynamicsymbols('phi_old p_old')
+velocity_equation_linear = sp.Eq(lhs=phi.diff(),rhs=p_old)
+roll_diff_equation_linear_subs = direct_linear_estimator.roll_diff_equation.subs(
+    [
+        (phi.diff(), p_old),
+        (phi, phi_old),
+
+    ]
+)
+solution = sp.solve(roll_diff_equation_linear_subs,(p_old.diff()))[0]
+acceleration_equation_linear = sp.Eq(lhs=phi.diff().diff(), rhs=solution)
+
+A,B = sp.symbols('A B')  # helpers
+A_equation = sp.Eq(lhs=A, rhs=-2*zeta/omega0)
+B_equation = sp.Eq(lhs=B, rhs=-1/(omega0**2))
+
+zeta_equation = sp.Eq(lhs=zeta, rhs=sp.solve(A_equation,zeta)[0])
+omega_equation = sp.Eq(lhs=omega0, rhs=sp.solve(B_equation,omega0)[0])
+
+##### Quadratic
+
+
+C = sp.symbols('C')  # helpers
+C_equation = sp.Eq(lhs=C, rhs=B*d)
+d_equation = sp.Eq(lhs=d, rhs=sp.solve(C_equation,d)[0])
+
+##### Quadratic improved
+quadratic_stiffness_equation = sp.Eq(GZ,GM*phi+dGM*sp.Abs(phi)*phi)
--- rolldecay/symbols.py
+++ rolldecay/symbols.py
@@ -2,12 +2,38 @@ import rolldecay.special_symbol as ss
 import sympy as sp
 import sympy.physics.mechanics as me
 
+import rolldecayestimators.special_symbol as ss
+import sympy as sp
+import sympy.physics.mechanics as me
+
 t = ss.Symbol(name='t',description='time',unit='s')
+I = ss.Symbol(name='I',description='total roll inertia',unit='kg*m**2')
+B = ss.Symbol(name='B',description='total roll damping',unit='kg*m*/s')
+rho = ss.Symbol(name='rho',description='water density',unit='kg/m3')
+g = ss.Symbol(name='g',description='acceleration of gravity',unit='m/s**2')
+Disp = ss.Symbol(name='Disp',description='displacement',unit='m**3')
+M_x = ss.Symbol(name='M_x',description='External roll moment',unit='Nm')
+m = ss.Symbol(name='m',description='mass of ship',unit='kg')
+GM = ss.Symbol(name='GM', description='metacentric height', unit='m')
+dGM = ss.Symbol(name='dGM', description='metacentric height correction', unit='m/rad')
+omega = ss.Symbol(name='omega', description='Frequency of external moment', unit='Nm')
 
 phi = me.dynamicsymbols('phi')  # Roll angle
+#phi = ss.Symbol(name='phi', description='Roll angle', unit='rad')  # Roll angle
 phi_dot = phi.diff()
 phi_dot_dot = phi_dot.diff()
 
 zeta = sp.Symbol('zeta') # Linear roll damping coefficeint
 omega0 = sp.Symbol('omega0')  # Natural roll frequency
 d = sp.Symbol('d')  # Nonlinear roll damping coefficient
+
+A_44 = ss.Symbol(name='A_44', description='General roll inertia', unit='kg*m**2')
+
+
+## Functions:
+GZ = sp.Function('GZ')(phi)
+B_44 = sp.Function('B_{44}')(phi_dot)
+C_44 = sp.Function('C_{44}')(phi)
+M_44 = sp.Function('M_{44}')(omega*t)
+
+
