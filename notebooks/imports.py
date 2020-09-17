d64cf63ba3d78bb48119f16ab9bafd21d21190c8
Martin Alexandersson <marale@kth.se>
2020-09-17 09:16:58 +0200

    Added a file with standard imports and setup for the notebooks. This one is used by using the %load magic.

---
+++ notebooks/imports.py
@@ -0,0 +1,30 @@
+"""
+These is the standard setup for the notebooks.
+"""
+
+%matplotlib inline
+%load_ext autoreload
+%autoreload 2
+
+from jupyterthemes import jtplot
+jtplot.style(theme='onedork', context='notebook', ticks=True, grid=False)
+
+import pandas as pd
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+pd.set_option("display.max_columns", None)
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+#plt.style.use('paper')
+
+#import data
+import copy
+from rolldecay.bis_system import BisSystem
+from rolldecay import database
+from mdldb.tables import Run
+
+from sklearn.pipeline import Pipeline
+from rolldecayestimators.transformers import CutTransformer, LowpassFilterDerivatorTransformer, ScaleFactorTransformer, OffsetTransformer
+from rolldecayestimators.direct_estimator_cubic import EstimatorQuadraticB, EstimatorCubic
+from rolldecayestimators.ikeda_estimator import IkedaQuadraticEstimator
